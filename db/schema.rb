6e16fdf2ce67a016bd5dfb88b03357e7b96732bf
Agnes Ching <agnesching19@gmail.com>
2017-11-13 15:06:06 +0000

    van model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113144339) do
+ActiveRecord::Schema.define(version: 20171113150221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,4 +36,23 @@ ActiveRecord::Schema.define(version: 20171113144339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vans", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "location"
+    t.boolean "availability"
+    t.text "description"
+    t.string "photo"
+    t.string "make"
+    t.string "model"
+    t.integer "bed"
+    t.integer "sleep"
+    t.boolean "bathroom"
+    t.boolean "kitchen"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_vans_on_user_id"
+  end
+
+  add_foreign_key "vans", "users"
 end
