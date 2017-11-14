443dde7e2d9e6273dd975869c32622514c39763c
Joyce Wong <wong.cy.joyce@gmail.com>
2017-11-14 18:09:30 +0000

    updated validations for van model

--- app/models/van.rb
+++ app/models/van.rb
@@ -2,6 +2,6 @@ class Van < ApplicationRecord
   belongs_to :user
   has_many :reviews
 
-  validates :location, :availability, :description, :photo, :make, :model, :bed, :sleep, :bathroom, :kitchen, :price, presence: true
+  validates :location, :description, :photo, :make, :model, :bed, :sleep, :price, presence: true
 end
 
