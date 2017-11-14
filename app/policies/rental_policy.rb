64c589d21ca97897597ea97883df40574f7585fd
Joe Alexander <ja5664@gmail.com>
2017-11-14 17:29:55 +0000

    Adding policies to the rental mode

---
+++ app/policies/rental_policy.rb
@@ -0,0 +1,19 @@
+class RentalPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(:user = user)
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
