34715fd404fd86d2af4857f338ffb76d127cdfd9
Joe Alexander <ja5664@gmail.com>
2017-11-17 12:39:33 +0000

    styling the rentals page

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,9 +1,35 @@
 class RentalsController < ApplicationController
     before_action :set_rental, only: [:show, :edit, :update, :destroy]
     before_action :set_van, only: [:new, :create]
+    before_action :set_user, only: [:index]
+    helper_method :weather_forecast
 
   def index
     @rentals = policy_scope(Rental).order(created_at: :desc)
+    @user_name = @user.first_name
+    weather_forecast
+
+    # Setting message based on trip count
+    if @rentals.count == 0
+      @status = "No trips booked yet - why not book one?"
+    elsif @rentals.count == 1
+      @status = "You have #{@rentals.count} trip booked!"
+    else
+      @status = "You have #{@rentals.count} trips booked!"
+    end
+
+    # Setting status for van owners
+    if Van.where(user_id: @user)
+      @user_owns_a_van = true
+    end
+
+    if @user_owns_a_van && Rental.where(user_id: @user).count > 0
+      @rented_message_one = "Your vans are rented #{Rental.where(user_id: @user).count} times"
+      @rented_message_two = "Superb - with the money from those you can have some beers!"
+    else
+      @rented_message_one = "No rentals just yet why edit your van description to make it even better!"
+      @rented_message_two = "Or on second thoughts take it on a road trip!"
+    end
   end
 
   def new
@@ -61,5 +87,24 @@ class RentalsController < ApplicationController
 
   def set_rental
     @rental = Rental.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def weather_forecast
+    # require 'json'
+    # require 'open-uri'
+    # key = ENV['OPEN_WEATHER_KEY']
+    # latitude = 20
+    # longitude = 20
+    # url = "http://api.openweathermap.org/data/2.5/forecast?lat=#{latitude}&lon=#{longitude}&APPID=#{key}"
+    # weather_serialized = open(url).read
+    # @forecast = JSON.parse(weather_serialized)
+    # @weather = ["#{@forecast["list"][7]["weather"][0]["main"]}","#{@forecast["list"][15]["weather"][0]["main"]}",
+    # "#{@forecast["list"][23]["weather"][0]["main"]}","#{@forecast["list"][31]["weather"][0]["main"]}",
+    # "#{@forecast["list"][39]["weather"][0]["main"]}"]
+    @weather = "sunny"
   end
 end
